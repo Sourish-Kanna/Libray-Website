114c1646ac0f5238ba913fffca135c0b2ec04fc2
suthakaranburaj <suthakaranburaj@gmail.com>
2024-08-29 19:49:24 +0530

    Home page also done

--- Frontend/src/Components/Pages/HomePage/HomePage.jsx
+++ Frontend/src/Components/Pages/HomePage/HomePage.jsx
@@ -11,15 +11,15 @@ library.add(fas);
 export default function HomePage() {
   return (
     //image section
-        <div className='mt-20 h-full w-full overflow-x-hidden font-serif '>
+        <div className='w-full h-full mt-20 overflow-x-hidden font-serif '>
             <Link to='/'></Link>
-            <div className='bg-homepage-bg h-screen bg-no-repeat w-full bg-center bg-cover'>
+            <div className='w-full h-screen bg-center bg-no-repeat bg-cover bg-homepage-bg'>
 
                 {/* accouncement */}
 
-                <div className='mx-40 pt-9 flex w-full'>
-                    <p className='bg-yellow-300 h-10 rounded-l-xl w-20 pl-2 font-bold text-red-600 text-2xl'>News</p>
-                    <div className='flex text-xl w-9/12  bg-yellow-100 rounded-r-xl justify-around'>
+                <div className='flex w-full mx-40 pt-9'>
+                    <p className='w-20 h-10 pl-2 text-2xl font-bold text-red-600 bg-yellow-300 rounded-l-xl'>News</p>
+                    <div className='flex justify-around w-9/12 text-xl bg-yellow-100 rounded-r-xl'>
                         <p>news 1</p>
                         <p>news 2</p>
                         <p>news 1</p>
@@ -32,23 +32,28 @@ export default function HomePage() {
                 </div>
 
                     {/* college name */}
-
-                <div className='text-9xl flex align-center h-8/12 justify-center mt-44 font-bold mx-40'>
-                        <p>SIES GST LIBRARY</p>
+                  
+                <div className='mx-40 mt-44'>
+                     <p className='font-bold text-white typing-demo text-9xl'>
+                            SIES GST LIBRARY
+                    </p>
                 </div>
+                {/* Typing Animation Styles */}
+                
 
+                
                      {/*links */}
 
-                <div className='text-3xl mr-40 flex mt-20 w-3/6  justify-around ml-96 pl-20 pr-32'>
-                    <div className='bg-red-500 rounded-xl '>PYQs <FontAwesomeIcon className='text-3xl py-1' icon="fa-solid fa-arrow-up-right-from-square" /></div>
-                    <div className='bg-red-500 rounded-xl'>Learn more <FontAwesomeIcon className='text-3xl py-1' icon="fa-solid fa-arrow-up-right-from-square" /></div>
+                <div className='flex justify-around w-3/6 pl-20 pr-32 mt-20 mr-40 text-3xl ml-96'>
+                    <div className='bg-red-500 rounded-xl '>PYQs <FontAwesomeIcon className='py-1 text-3xl' icon="fa-solid fa-arrow-up-right-from-square" /></div>
+                    <div className='bg-red-500 rounded-xl'>Learn more <FontAwesomeIcon className='py-1 text-3xl' icon="fa-solid fa-arrow-up-right-from-square" /></div>
                 </div>
             </div>
 
                                             {/*Overview */}
-            <div className='mx-40 p-10 mb-10'>
+            <div className='p-10 mx-40 mb-10'>
                 <div >
-                    <div className='flex justify-center font-bold text-4xl'>
+                    <div className='flex justify-center text-4xl font-bold'>
                     <p>Overview</p>
                     </div>
                     <div class=" border-b-4 mx-auto w-36 mt-2 border-blue-700 mb-10"></div>
@@ -63,12 +68,12 @@ export default function HomePage() {
 
             <div className='mx-40 mb-10'>
                 <div >
-                    <div className='flex justify-center font-bold text-4xl'>
+                    <div className='flex justify-center text-4xl font-bold'>
                     <p>Images</p>
                     </div>
                     <div class=" border-b-4 mx-auto w-28 mt-2 border-blue-700 mb-10"></div>
                 </div>
-                <div className=' flex justify-between '>
+                <div className='flex justify-between '>
                 <img className="h-32 ml-4" src={booksof_library} alt="" />
                 <img className="h-32 ml-4" src={booksof_library} alt="" />
                 <img className="h-32 ml-4" src={booksof_library} alt="" />
@@ -78,26 +83,26 @@ export default function HomePage() {
 
                                                 {/* Librarian */}
 
-            <div className=' mb-10 text-xl'>
+            <div className='mb-10 text-xl '>
                 <div>
                     <div >
-                        <div className='flex justify-center font-bold text-4xl'>
+                        <div className='flex justify-center text-4xl font-bold'>
                         <p>Librarian</p>
                         </div>
                         <div class=" border-b-4 mx-auto w-36 mt-2 border-blue-700 mb-10"></div>
                     </div>
                 </div>
-                <div className='flex justify-between bg-blue-500 w-full'>
-                    <div className='flex-col  ml-40'>
+                <div className='flex justify-between w-full bg-blue-500'>
+                    <div className='flex-col ml-40'>
                         <img className='rounded-full h-44' src={Library_office_photo} alt="" />
                         <p>SIES GST Librarian</p>
                         <p>Phone no</p>
                         <p>email</p>
                     </div>
-                    <div className='w-6/12 flex items-center'>
+                    <div className='flex items-center w-6/12'>
                         <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, non? Similique, fuga? Asperiores provident voluptate minima assumenda et temporibus illo nam laudantium. Sequi eius modi asperiores ipsam consequatur autem excepturi laudantium incidunt magnam! Nostrum maxime ipsa ab necessitatibus, dolorum quod incidunt magnam harum qui provident?</p>
                     </div>
-                    <div className='mr-40 mt-20 text-xl'>
+                    <div className='mt-20 mr-40 text-xl'>
                         <p>Staffs</p>
                         <p>Rules & Regulations</p>
                     </div>
@@ -109,7 +114,7 @@ export default function HomePage() {
             
             <div className='mx-40'>
                 <div >
-                    <div className='flex justify-center font-bold text-4xl'>
+                    <div className='flex justify-center text-4xl font-bold'>
                     <p>Explore Policies</p>
                     </div>
                     <div class=" border-b-4 mx-auto w-28 mt-2 border-blue-700 mb-10"></div>
