cea5a9b2aca3c2ff51c7880c97842a8def6aa15b
Maharajan <0604maharaja@gmail.com>
2024-09-07 20:55:06 +0530

    hours and staffs changed

--- Frontend/src/Components/Pages/AboutUs/AboutUs.jsx
+++ Frontend/src/Components/Pages/AboutUs/AboutUs.jsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
 import Images from '../HomePage/assets/img5.webp'
-
+import sand from '../HomePage/assets/sandip.webp'
+import indra from '../HomePage/assets/indrabahadur.webp'
+import sac from '../HomePage/assets/sachin.webp'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, faClock);
+library.add(fas);
 export default function AboutUs() {
     return (
         <div className='font-serif mt-28'>
@@ -19,7 +27,7 @@ export default function AboutUs() {
                             {/* Library hours */}
 
             <div>
-                <div className='h-40 flex items-center justify-center'>
+                <div className='h-28 flex items-center justify-center'>
                     <div >
                     <div className='flex justify-center text-4xl font-bold '>
                         <p>Library-Hours</p>
@@ -28,13 +36,26 @@ export default function AboutUs() {
                     </div>
                 </div>
         
-                <div className="h-screen mx-40 my-0.5 bg-[#f3f2ed] shadow-2xl flex items-center rounded-xl">
-                    <div className="w-1/2 rounded-2xl">
-                        <img src={Images} className="h-60 p-6 rounded-2xl "></img>
+                <div className="h-96 mx-40 my-0.5 bg-[#f3f2ed] shadow-2xl flex items-center rounded-xl hover:scale-105 duration-700">
+                    <div className="m-20 w-5/12 rounded-2xl ">
+                        <img src={Images} className="h-60 rounded-2xl "></img>
                     </div>
-                    <div className='w-1/2 bg-yellow-50'>
-                        <li>Operating hours</li>
-                        <li>saturdays</li>
+                    <div className='w-7/12 mr-10 p-6 '>
+                    <ul className='list-none'>
+                        
+                        <li className="text-2xl font-bold text-orange-500">
+                        <FontAwesomeIcon icon="fa-regular fa-clock" className="mr-2 text-blue-500" />
+                            OPERATING HOURS</li>
+                        <li className="ml-8 font-semibold">
+                            9:00 AM to 6:00 PM</li>
+                        <li className='ml-8 font-semibold'>Monday to Friday</li>
+                        <li className='mt-4 text-2xl font-bold text-orange-500'>
+                        <FontAwesomeIcon icon="fa-regular fa-clock" className="mr-2 text-blue-500" />
+                            SATURDAYS</li>
+                        <li className='ml-8 font-semibold'>
+                            9:00 AM to 2:00 PM</li>
+                        <li className='mt-4 text-3xl font-bold text-orange-500'>Closed on Sundays and public holidays</li>
+                        </ul>
                     </div>
                 
             
@@ -44,7 +65,7 @@ export default function AboutUs() {
             </div>
                                         {/* Library Staffs */}
             <div>
-                <div className='w-full h-40 flex items-center justify-center'>
+                <div className='w-full h-36 flex items-center justify-center'>
                     <div >
                         <div className='flex justify-center text-4xl font-bold '>
                             <p>Library Staffs</p>
@@ -52,11 +73,40 @@ export default function AboutUs() {
                         <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
                     </div>
                 </div>
-                <div className="h-screen mx-40 mt my-0.5 bg-[#f3f2ed] shadow-2xl flex flex-col items-center ">
-                <div className="flex flex-row space-x-10"></div>
-                    <div className='h-32 w-32mx-10 mt-10  bg-black shadow-2xl'></div>
-                        <p>hello</p></div>
-            </div>
+                <div className="h-screen mx-40 mt my-0.5 bg-blue-500 shadow-2xl flex rounded-xl items-center hover:scale-105 duration-700 ">
+                    <div className="m-20 w-56 mr-16 ">
+                    <img src={sand} className="h-60 w-56 rounded-2xl"></img>
+                    <div className="bg-white p-8 mt-10 rounded-xl ">
+                        <ul>
+                        <li className="text-2xl font-extrabold">Library Clerk</li>
+                        <li className="font-bold ml-6">Mr.Sandip Patil</li> 
+                        <li className="font-semibold ml-3"> M.A,B.Ed.,M.L.I.Sc</li>
+                        </ul>
+                    </div>
+                    </div> 
+                    <div className="m-20 w-5/12 mr-10">
+                    <img src={sac} className="h-60 w-56 rounded-2xl "></img>
+                    <div className="bg-white p-8 mt-10 w-64 rounded-xl">
+                        <ul>
+                        <li className="text-2xl font-extrabold">Library Attendant</li>
+                        <li className="font-bold ml-6">Mr.Sachin Gurav</li> 
+                        <li className="font-semibold ml-5"> M.A,B.Ed.,M.L.I.Sc</li>
+                        </ul>
+                    </div>
+                    </div>
+                    <div className="m-20  w-5/12 ml-16">
+                    <img src={indra} className="h-60 w-56 rounded-2xl"></img>
+                    <div className="bg-white p-8 mt-10 w-64 rounded-xl ">
+                        <ul>
+                        <li className="text-2xl font-extrabold">Library Attendant</li>
+                        <li className="font-bold ml-4">Mr.Indrabahadur singh</li> 
+                        <li className="font-semibold ml-20"> 11th</li>
+                        </ul>
+                    </div>
+                    
+                    </div>
+                </div>
+            </div>    
                                         {/* Facilities */}
             <div>
                 <div className='w-full h-40 flex items-center justify-center'>
