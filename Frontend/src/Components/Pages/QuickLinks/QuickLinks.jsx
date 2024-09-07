584de6b298975027584b9088bb018f4c99934151
Sourish Kanna <sourishkanna001@gmail.com>
2024-09-07 19:30:31 +0530

    Navbar Routing

--- Frontend/src/Components/Pages/QuickLinks/QuickLinks.jsx
+++ Frontend/src/Components/Pages/QuickLinks/QuickLinks.jsx
@@ -4,123 +4,78 @@ import { Link } from 'react-router-dom'
 export default function Services() {
   return (
 
+    <div className='font-serif mt-28'>
+        <Link to='/services'>
+        </Link>
+          <div className='w-full h-48 bg-[#efefef] flex items-center justify-center'>
+            <div >
+              <div className='flex justify-center text-6xl font-bold text-[#014da1]'>
+                <p>Quick Links</p>
+              </div>
+              <div className=" border-b-4 mx-auto w-44 mt-2 border-red-600 mb-10"></div>
+              </div>
+          </div>
+
+                                                  {/* Question Banks */}
+
+                                                  <div>
+            <div className='w-full h-60  flex items-center justify-center'>
+              <div >
+                  <div className='flex justify-center text-4xl font-bold '>
+                      <p>Question Banks</p>
+                  </div>
+                  <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
+              </div>
+            </div>
+          </div>
+                                        {/* Library Brochure */}
+            <div>
+                <div className='w-full h-60  flex items-center justify-center'>
+                    <div >
+                        <div className='flex justify-center text-4xl font-bold '>
+                            <p>Library Brochure</p>
+                        </div>
+                        <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
+                    </div>
+                </div>
+            </div>
+                                        {/* University Syllabus*/}
+            <div>
+                <div className='w-full h-60  flex items-center justify-center'>
+                    <div >
+                        <div className='flex justify-center text-4xl font-bold '>
+                            <p>University Syllabus</p>
+                        </div>
+                        <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
+                    </div>
+                </div>
+            </div>
+                                        {/*Scholarships*/}
+            <div>
+                <div className='w-full h-60  flex items-center justify-center'>
+                    <div >
+                        <div className='flex justify-center text-4xl font-bold '>
+                            <p>Scholarships</p>
+                        </div>
+                        <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
+                    </div>
+                </div>
+            </div>
+                                        {/*Research Funds*/}
+            <div>
+                <div className='w-full h-60  flex items-center justify-center'>
+                    <div >
+                        <div className='flex justify-center text-4xl font-bold '>
+                            <p>Research Funds</p>
+                        </div>
+                        <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
+                    </div>
+                </div>
+            </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // <div className='font-serif mt-28'>
-    //     <Link to='/services'>
-    //     </Link>
-    //       <div className='w-full h-48 bg-[#efefef] flex items-center justify-center'>
-    //         <div >
-    //           <div className='flex justify-center text-6xl font-bold text-[#014da1]'>
-    //             <p>Quick Links</p>
-    //           </div>
-    //           <div className=" border-b-4 mx-auto w-44 mt-2 border-red-600 mb-10"></div>
-    //           </div>
-    //       </div>
-
-    //                                               {/* Question Banks */}
-
-    //                                               <div>
-    //         <div className='w-full h-60  flex items-center justify-center'>
-    //           <div >
-    //               <div className='flex justify-center text-4xl font-bold '>
-    //                   <p>Question Banks</p>
-    //               </div>
-    //               <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //                                     {/* Library Brochure */}
-    //         <div>
-    //             <div className='w-full h-60  flex items-center justify-center'>
-    //                 <div >
-    //                     <div className='flex justify-center text-4xl font-bold '>
-    //                         <p>Library Brochure</p>
-    //                     </div>
-    //                     <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //                                     {/* University Syllabus*/}
-    //         <div>
-    //             <div className='w-full h-60  flex items-center justify-center'>
-    //                 <div >
-    //                     <div className='flex justify-center text-4xl font-bold '>
-    //                         <p>University Syllabus</p>
-    //                     </div>
-    //                     <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //                                     {/*Scholarships*/}
-    //         <div>
-    //             <div className='w-full h-60  flex items-center justify-center'>
-    //                 <div >
-    //                     <div className='flex justify-center text-4xl font-bold '>
-    //                         <p>Scholarships</p>
-    //                     </div>
-    //                     <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //                                     {/*Research Funds*/}
-    //         <div>
-    //             <div className='w-full h-60  flex items-center justify-center'>
-    //                 <div >
-    //                     <div className='flex justify-center text-4xl font-bold '>
-    //                         <p>Research Funds</p>
-    //                     </div>
-    //                     <div className=" border-b-4 mx-auto w-44 mt-2 border-blue-700 mb-10"></div>
-    //                 </div>
-    //             </div>
-    //         </div>
-
-
-
-        {/* <p className='text-4xl font-bold my-10 border-b-4  border-orange-500 pb-4 h-14  text-center'>Library Services</p>
+{/* 
+        <p className='text-4xl font-bold my-10 border-b-4  border-orange-500 pb-4 h-14  text-center'>Library Services</p>
         <div className="Flex m-8 font-serif" >
         <h1 className='text-4xl'>Library provides the following Services:</h1>
           <div className='flex m-8 justify-between '>
@@ -142,6 +97,6 @@ export default function Services() {
           </div>
         </div> */}
       
-    // </div>
+    </div>
   )
 }
