840fc461f7e114843c16af1abc97060fa0b97bdf
Sourish Kanna <sourishkanna001@gmail.com>
2024-10-19 02:14:19 +0530

    Header basic responsive done

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -3,6 +3,9 @@ import { StyledNavLink } from './Navigation.jsx'
 // import { Link } from 'react-router-dom'
 import Sies_logo from './assets/sies_logo_header.png'
 // import user from './assets/user_icon.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 
 const Scholarship = "https://scholarships.gov.in/"
@@ -11,15 +14,76 @@ const activeClassName = 'text-[#f26d21]';
 const inactiveClassName = 'text-[#014da1] hover:text-blue-400';
 
 function NavBar() {
-    return (
-      <div className='fixed top-0 z-20 flex w-full px-30 h-fit bg-Navbg'>
+    const [isOpen, setIsOpen] = useState(false);
 
-            <div className='flex-grow'>
-                <img src={Sies_logo} alt="SIES Logo" className="h-28" />
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+      <div className='fixed top-0 z-20 w-full h-fit bg-Navbg'>
+
+            <div className='flex items-center justify-between px-4 lg:px-10 xl:px-30 '>
+                <div className='flex items-center'>
+                    <img src={Sies_logo} alt="SIES Logo" className="h-full" />
+                </div>
+
+            {/* Hamburger Icon (visible on small screens) */}
+            <div className='flex items-center lg:hidden'>
+                    <button onClick={toggleMenu} className="text-xl text-[#014da1]">
+                        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="lg" />
+                    </button>
+                </div>
+
+            {/* Desktop Menu (hidden on small screens) */}
+            <div className='hidden lg:flex-grow lg:flex items-center justify-between w-full mt-2 pl-48'>
+                <ul className='flex items-center justify-between w-full font-serif flex-nowrap'>
+
+                    <li className='text-[#014da1] cursor-pointer font-medium text-xl mr-4'>
+                        <NavLink to='' className={({ isActive }) => 
+                            `${isActive ? activeClassName : inactiveClassName}`
+                            }>Home</NavLink>
+                    </li>
+                    
+                    <StyledNavLink 
+                    to="/academics" 
+                    text="E-Resources" 
+                    drop_link = {["/quicklinks#pyq","/academics","/academics#university-syllabus","/academics#academic-calender","/academics#competitive-exams"]}
+                    drop_name={["Question Papers","OER","DELNET","IEEE","OPAC"]}/>
+                    
+                    <StyledNavLink 
+                    to="/about" 
+                    text="About Us" 
+                    drop_link = {["/about#library-hours","/about#library-hours","/about#library-staffs","/about#infrastructure"]}
+                    drop_name={["Library Hours","Libray Rules","Library Staffs","FAQs"]}/>
+
+                    {/* <StyledNavLink 
+                    to="/quicklinks" 
+                    text="Quick Links" 
+                    drop_link = {["/quicklinks#pyq","/donate-books","/suggest-books",Scholarship,Research_fund,"/quicklinks#lib-brochure"]} 
+                    drop_name={["PYQs","Donate Books","Suggest Books","Scholarships", "Research Funds", "Library Brochure"]}/> */}
+                    
+                    <StyledNavLink 
+                    to="/others" 
+                    text="Others" 
+                    drop_link = {["/donate-books","/suggest-books","/others","/others","/about#facilities","/about#infrastructure"]}
+                    drop_name={["Donate Books","Suggest Books","Book Bank","Feedback","Facilities","Infrastructure"]}/>
+
+                    <li className='text-[#014da1] cursor-pointer font-medium text-xl mr-4'>
+                        <NavLink to='contactus' className={({ isActive }) => 
+                            `${isActive ? activeClassName : inactiveClassName}`
+                            }>Contact Us</NavLink>
+                    </li>
+
+                </ul>
+            </div>
             </div>
 
-            <div className='flex-grow h-full mt-2'>
-                <ul className='flex items-center justify-between w-full font-serif flex-nowrap'>
+            {/* Mobile Menu (only visible when the hamburger is open) */}
+            {isOpen && (
+                <div className='lg:hidden'>
+                    <ul className='flex flex-col items-start px-4 py-2 space-y-2'>
+                        
                     <li className='text-[#014da1] cursor-pointer font-medium text-xl m-8'>
                         <NavLink to='' className={({ isActive }) => 
                             `${isActive ? activeClassName : inactiveClassName}`
@@ -50,14 +114,15 @@ function NavBar() {
                     drop_link = {["/donate-books","/suggest-books","/others","/others","/about#facilities","/about#infrastructure"]}
                     drop_name={["Donate Books","Suggest Books","Book Bank","Feedback","Facilities","Infrastructure"]}/>
 
-                    <li className='text-[#014da1] cursor-pointer font-medium text-xl mr-8'>
+                    <li className='text-[#014da1] cursor-pointer font-medium text-xl m-8'>
                         <NavLink to='contactus' className={({ isActive }) => 
                             `${isActive ? activeClassName : inactiveClassName}`
                             }>Contact Us</NavLink>
                     </li>
 
                 </ul>
-            </div>
+                </div>
+            )}
 
             {/* <div className='flex align-center h-12 w-40 bg-orange-400 justify-center rounded-full mt-8 '>
                 <Link to='/register'><p className='my-3 '>Register</p></Link>
@@ -66,6 +131,7 @@ function NavBar() {
             <div className='flex items-center'>
                 <img src={user} className='h-12 w-12 mb-4 mx-5 ' alt="" />
             </div> */}
+
             
         </div>
     )
