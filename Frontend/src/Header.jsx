4e420a7074bc42b28da9347d4554659caeb413ed
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-01-06 13:46:31 +0530

    contact us page and login button redesign done!

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -121,24 +121,21 @@ function NavBar() {
             </li>
 
             <li>
-              {/* <div className="w-full h-full border-black	bg-orange-500 rounded-full flex	">
-                            <button className="bg-white w-17 h-20 rounded-full m-5  ">login </button>
-                        </div> */}
-              <div class="flex items-center  bg-[#f3f2ed] font-serif rounded-full px-4 py-2 shadow-2xl border-2	 border-b-2	 border-[#014da1] hover:border-[#f26d21] ">
-                <button class=" text-[#014da1] font-medium mr-4 text-xl hover: active:text-[#f26d21]">
-                  <NavLink
-                    to="Login"
-                    className={({ isActive }) =>
-                      `${isActive ? activeClassName : inactiveClassName}`
-                    }
-                  >
-                    Login
-                  </NavLink>
-                </button>
-                <div class="w-12 h-12 bg-gray-400 hover:cursor-pointer rounded-full flex items-center justify-center text-white text-lg">
-                  Pic
+              <NavLink
+                to="Login"
+                className={({ isActive }) =>
+                  `flex items-center bg-[#f3f2ed] font-serif rounded-full px-4 py-2 shadow-2xl border-2 border-b-2 border-[#014da1] hover:border-[#f26d21] ${
+                    isActive ? activeClassName : inactiveClassName
+                  }`
+                }
+              >
+                <div class="text-[#014da1] font-medium mr-4 text-xl hover:active:text-[#f26d21]">
+                  Login
                 </div>
-              </div>
+                <button class="w-12 h-12 bg-gray-400 hover:cursor-pointer rounded-full flex items-center justify-center text-white text-lg">
+                  Pic
+                </button>
+              </NavLink>
             </li>
           </ul>
         </div>
