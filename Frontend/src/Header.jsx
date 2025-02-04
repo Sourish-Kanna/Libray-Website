52043e1230c704c71db08c3a7ecd6842a91525f6
Suthakar Anburaj <suthakaranburaj@gmail.com>
2025-02-05 00:51:45 +0530

    navbar view changed for mobile view

--- Frontend/src/Header.jsx
+++ Frontend/src/Header.jsx
@@ -30,24 +30,23 @@ const navLinks = [
   },
 ];
 
-const StyledNavLink = ({ to, text, drop_link, drop_name, isMobile}) => {
-    if (isMobile) {
-        return (
-            <li className="text-s_blue group cursor-pointer font-medium text-2-Primary mt-8 flex items-center">
-              <NavLink
-              to={to}
-              className={({ isActive }) => `${isActive ? activeClassName : inactiveClassName}`}>
-                {text}
-                <FontAwesomeIcon icon={faCaretDown} className="px-1.5" />
-              </NavLink>
-                <MobStyledDropdown
-                  to={drop_link}
-                  text={drop_name}
-                />
-              {!isMobile && <StyledDropdown to={drop_link} text={drop_name} />}
-            </li>
-          );
-    }
+const StyledNavLink = ({ to, text, drop_link, drop_name, isMobile }) => {
+  if (isMobile) {
+    return (
+      <li className="text-s_blue group cursor-pointer font-medium text-2-Primary mt-8 flex items-center relative">
+        <NavLink
+          to={to}
+          className={({ isActive }) => `${isActive ? activeClassName : inactiveClassName}`}>
+          {text}
+          <FontAwesomeIcon icon={faCaretDown} className="px-1.5" />
+        </NavLink>
+        <MobStyledDropdown
+          to={drop_link}
+          text={drop_name}
+        />
+      </li>
+    );
+  }
   return (
     <li className="text-s_blue group relative cursor-pointer font-medium text-Primary m-8 flex items-center">
       <NavLink
@@ -77,19 +76,19 @@ const StyledDropdown = ({ to, text }) => {
   );
 };
 
-const MobStyledDropdown = ({ to, text}) => {
-    return (
-        <div className=" hidden group-hover:flex w-52 top-8 -left-14">
-          <ul className="bg-header-color text-center">
-            {text.map((item, index) => (
-              <li key={index} className="py-3 pl-2 border-b-2 hover:bg-gray-200">
-                <CustomLink link={to[index]} name={item} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-  };
+const MobStyledDropdown = ({ to, text }) => {
+  return (
+    <div className="ml-4 hidden group-hover:block fixed left-3/4 transform -translate-x-1/2 top-14 w-2/4 bg-header-color z-50 shadow-lg rounded-lg">
+      <ul className="text-center">
+        {text.map((item, index) => (
+          <li key={index} className="py-3 pl-2 border-b-2 hover:bg-gray-200">
+            <CustomLink link={to[index]} name={item} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const CustomLink = ({ name, link }) => {
   if (link.startsWith('/')) {
@@ -99,18 +98,19 @@ const CustomLink = ({ name, link }) => {
   }
 };
 
+
+
 function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
-    const { login, isLoading, error, isAuthenticated,user,logoutUser } = useAuthStore();
+  const { login, isLoading, error, isAuthenticated, user, logoutUser } = useAuthStore();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-
-    const submitHandler = () => {
-      logoutUser();
-    };
+  const submitHandler = () => {
+    logoutUser();
+  };
 
   return (
     <nav className="top-0 z-20 w-full h-fit bg-header-color bg-header-bg bg-top-header bg-cover-header">
@@ -151,93 +151,92 @@ function NavBar() {
               </NavLink>
             </li>
 
-              <li>
-                <NavLink
-                  to={isAuthenticated ? "#" : "Login"} // Prevent navigation when logging out
-                  onClick={isAuthenticated ? submitHandler : null} // Call logout if authenticated
-                  className={({ isActive }) =>
-                    // `flex items-center font-serif rounded-full px-4 py-2 shadow-2xl border-2 border-b-2 `
-                    // `${isAuthenticated ? "bg-orange-500" : "bg-[#f3f2ed]"} `
-                    // border-s_blue hover:border-s_orange
-                    `${isActive ? activeClassName : inactiveClassName}`
-                  }
-                >
-                  <div className="flex items-center w-full cursor-pointer bg-header-color rounded-full px-4 py-2 shadow-md  border-2
-                 border-s_blue hover:border-s_orange transform transition duration-500 ease-in-out">
-                    <span className={`${isAuthenticated ? "text-s_blue" : "text-orange-500"} font-medium mr-4 text-xl hover:active:text-[#f26d21] text-s_blue hover:text-s_orange active:text-s_orange_400" `}>
-                      {isAuthenticated ? "Logout" : "Login"}
-                    </span>
-                    <div className="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center overflow-hidden hover:cursor-pointer text-white text-lg">
-                      <img
-                        src={isAuthenticated && user?.avatar ? user.avatar : image1}
-                        className="profile-image w-full h-full object-cover"
-                        alt=""
-                      />
-                    </div>
+            <li>
+              <NavLink
+                to={isAuthenticated ? "#" : "Login"} // Prevent navigation when logging out
+                onClick={isAuthenticated ? submitHandler : null} // Call logout if authenticated
+                className={({ isActive }) =>
+                  `${isActive ? activeClassName : inactiveClassName}`
+                }
+              >
+                <div className="flex items-center w-full cursor-pointer bg-header-color rounded-full px-4 py-2 shadow-md  border-2 border-s_blue hover:border-s_orange transform transition duration-500 ease-in-out">
+                  <span className={`${isAuthenticated ? "text-s_blue" : "text-orange-500"} font-medium mr-4 text-xl hover:active:text-[#f26d21] text-s_blue hover:text-s_orange active:text-s_orange_400" `}>
+                    {isAuthenticated ? "Logout" : "Login"}
+                  </span>
+                  <div className="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center overflow-hidden hover:cursor-pointer text-white text-lg">
+                    <img
+                      src={isAuthenticated && user?.avatar ? user.avatar : image1}
+                      className="profile-image w-full h-full object-cover"
+                      alt=""
+                    />
                   </div>
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+                </div>
+              </NavLink>
+            </li>
+          </ul>
         </div>
+      </div>
 
       {/* Mobile Menu (only visible when the hamburger is open) */}
       {isOpen && (
-        <div className="lg:hidden">
-          <ul className="flex flex-col items-center px-4 py-2 space-y-2">
-            <li className="text-s_blue cursor-pointer font-medium text-2-Primary mt-8">
-              <NavLink to="" className={({ isActive }) => `${isActive ? activeClassName : inactiveClassName}`}>
-                Home
-              </NavLink>
-            </li>
+  <div className="lg:hidden fixed inset-0 z-30 flex">
+    {/* Semi-transparent overlay */}
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50"
+      onClick={toggleMenu}
+    ></div>
 
-            {navLinks.map((link, index) => (
-              <StyledNavLink
-                key={index}
-                to={link.to}
-                text={link.text}
-                drop_link={link.drop_link}
-                drop_name={link.drop_name}
-                isMobile={true} />
-            ))}
+    {/* Mobile Menu */}
+    <div className="fixed left-0 top-0 h-full w-1/2 bg-header-color z-40 overflow-y-auto">
+      <ul className="flex flex-col items-center px-4 py-2 space-y-2">
+        <li className="text-s_blue cursor-pointer font-medium text-2-Primary mt-8">
+          <NavLink to="" className={({ isActive }) => `${isActive ? activeClassName : inactiveClassName}`}>
+            Home
+          </NavLink>
+        </li>
 
-            <li className="text-s_blue cursor-pointer font-medium text-2-Primary mx-8">
-              <NavLink to="contactus" className={({ isActive }) => `${isActive ? activeClassName : inactiveClassName}`}>
-                Contact Us
-              </NavLink>
-            </li>
+        {navLinks.map((link, index) => (
+          <StyledNavLink
+            key={index}
+            to={link.to}
+            text={link.text}
+            drop_link={link.drop_link}
+            drop_name={link.drop_name}
+            isMobile={true} />
+        ))}
 
-            <li>
-                <NavLink
-                  to={isAuthenticated ? "#" : "Login"} // Prevent navigation when logging out
-                  onClick={isAuthenticated ? submitHandler : null} // Call logout if authenticated
-                  className={({ isActive }) =>
-                    // `flex items-center font-serif rounded-full px-4 py-2 shadow-2xl border-2 border-b-2 `
-                    // `${isAuthenticated ? "bg-orange-500" : "bg-[#f3f2ed]"} `
-                    // border-s_blue hover:border-s_orange
-                    `${isActive ? activeClassName : inactiveClassName}`
-                  }
-                >
-                  <div className="flex items-center w-full cursor-pointer bg-header-color rounded-full px-4 py-2 shadow-md  border-2
-                 border-s_blue hover:border-s_orange transform transition duration-500 ease-in-out">
-                    <span className={`${isAuthenticated ? "text-s_blue" : "text-orange-500"} font-medium mr-4 text-xl hover:active:text-[#f26d21] text-s_blue hover:text-s_orange active:text-s_orange_400" `}>
-                      {isAuthenticated ? "Logout" : "Login"}
-                    </span>
-                    <div className="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center overflow-hidden hover:cursor-pointer text-white text-lg">
-                      <img
-                        src={isAuthenticated && user?.avatar ? user.avatar : image1}
-                        className="profile-image w-full h-full object-cover"
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </NavLink>
-              </li>
-              
+        <li className="text-s_blue cursor-pointer font-medium text-2-Primary mx-8">
+          <NavLink to="contactus" className={({ isActive }) => `${isActive ? activeClassName : inactiveClassName}`}>
+            Contact Us
+          </NavLink>
+        </li>
 
-          </ul>
-        </div>
-      )}
+        <li>
+          {/* <NavLink
+            to={isAuthenticated ? "#" : "Login"} // Prevent navigation when logging out
+            onClick={isAuthenticated ? submitHandler : null} // Call logout if authenticated
+            className={({ isActive }) =>
+              `${isActive ? activeClassName : inactiveClassName}`
+            }
+          >
+            <div className="flex items-center w-full cursor-pointer bg-header-color rounded-full px-4 py-2 shadow-md  border-2 border-s_blue hover:border-s_orange transform transition duration-500 ease-in-out">
+              <span className={`${isAuthenticated ? "text-s_blue" : "text-orange-500"} font-medium mr-4 text-xl hover:active:text-[#f26d21] text-s_blue hover:text-s_orange active:text-s_orange_400" `}>
+                {isAuthenticated ? "Logout" : "Login"}
+              </span>
+              <div className="w-12 h-12 bg-gray-400 rounded-full flex items-center justify-center overflow-hidden hover:cursor-pointer text-white text-lg">
+                <img
+                  src={isAuthenticated && user?.avatar ? user.avatar : image1}
+                  className="profile-image w-full h-full object-cover"
+                  alt=""
+                />
+              </div>
+            </div>
+          </NavLink> */}
+        </li>
+      </ul>
+    </div>
+  </div>
+)}
     </nav>
   );
 }
