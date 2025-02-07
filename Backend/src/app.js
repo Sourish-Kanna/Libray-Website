adc220b6dbdeef4f3745570da856ec4ac47e3788
suthakaranburaj <suthakaranburaj@gmail.com>
2025-02-07 10:30:55 +0530

    cors urls updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,6 +10,8 @@ app.use(cors({
             'http://localhost:5173', 
             'https://libray-website-client.vercel.app',
             'https://libray-website.vercel.app',
+            'https://libray-website-71gt.vercel.app',
+            'https://libray-website-tan.vercel.app',
         ],
     credentials: true
 }))
