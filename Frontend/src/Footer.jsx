eb92dcf2f888c7be0e5b5f99f31e395a6e092832
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-06-06 12:09:41 +0530

    hey

--- Frontend/src/Footer.jsx
+++ Frontend/src/Footer.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faFacebook, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'; 
 import Sies_logo from './assets/Footer/sies_logo_footer.png';
 import { NavLink, Link } from 'react-router-dom';
-
+//Help iam stuck
 const activeClassName = 'text-s_orange';
 const inactiveClassName = "text-white hover:text-s_orange_400";
 
@@ -53,17 +53,17 @@ const socialIcons = [
 function Footer() {
 
   return (
-    <div className="py-12 bg-s_blue overflow-x-hidden">
+    <div className="py-12 overflow-x-hidden bg-s_blue">
       <div className="px-6 lg:mx-44">
-        <div className="flex flex-wrap justify-between items-start lg:items-center w-full">
+        <div className="flex flex-wrap items-start justify-between w-full lg:items-center">
 
-          <div className="flex flex-col items-center lg:items-start mb-8 lg:mb-0">
+          <div className="flex flex-col items-center mb-8 lg:items-start lg:mb-0">
             <img src={Sies_logo} alt="SIES Logo" className="h-full" />
             <div className="flex space-x-6">
               {socialIcons.map((social) => (
                 <a key={social.link} href={social.link} target="_blank" rel="noopener noreferrer" aria-label={`Visit ${social.link}`}>
                   <FontAwesomeIcon 
-                    className="text-3xl p-3 lg:text-4xl text-white hover:text-s_orange duration-700" 
+                    className="p-3 text-3xl text-white duration-700 lg:text-4xl hover:text-s_orange" 
                     icon={social.icon} 
                   />
                 </a>
@@ -72,7 +72,7 @@ function Footer() {
           </div>
 
           {/* Render sections dynamically */}
-          <div className="flex flex-cols-1 gap-8 lg:gap-16 lg:flex lg:space-x-16">
+          <div className="flex gap-8 flex-cols-1 lg:gap-16 lg:flex lg:space-x-16">
             {sections.map((section) => (
               <div key={section.title} className="py-6">
                 <NavLink 
@@ -82,7 +82,7 @@ function Footer() {
                 >
                   <p className="mb-4 font-serif font-bold text-center hover:text-s_orange_400 lg:text-left">{section.title}</p>
                 </NavLink>
-                <ul className="font-serif space-y-2">
+                <ul className="space-y-2 font-serif">
                   {section.links.map((link) => (
                     <Link key={link.name} to={link.path} aria-label={`Go to ${link.name}`}>
                       <li>
