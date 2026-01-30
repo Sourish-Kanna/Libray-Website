9754004ecd4384b1207e22ecfba954f461f99589
Sourish Kanna <115524993+Sourish-Kanna@users.noreply.github.com>
2026-01-31 01:16:55 +0530

    fix: update MongoDB connection string and upgrade ESLint dependencies
    - Updated MongoDB connection string in `Backend/src/db/index.js` to include `appName=SIESGST_Library`.
    - Upgraded ESLint and related packages in `Frontend/package-lock.json` and `Frontend/package.json` to the latest versions for improved linting and compatibility.

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () =>{
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}?appName=SIESGST_Library`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     }
     catch(error){
