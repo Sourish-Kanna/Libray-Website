1e0b135aaa2e0651a80d2efbc4319e296bd634ac
Sourish Kanna <115524993+Sourish-Kanna@users.noreply.github.com>
2026-01-31 17:19:24 +0530

    feat: add render configuration for Libray-Website and update API base URL comments

--- Frontend/src/Store/baseapi.js
+++ Frontend/src/Store/baseapi.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://library-siesgst.onrender.com/api/v1";
+const API_BASE_URL = "https://library-siesgst.onrender.com/api/v1"; // old url
+// const API_BASE_URL = "https://libray-website-vasp.onrender.com/api/v1"; // new url
 // const API_BASE_URL = "http://localhost:8000/api/v1";
 
 export { API_BASE_URL };
